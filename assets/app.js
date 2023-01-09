61d961ed195b2671fbf0f031ced9ce88f606f164
wolfmarcos <estacread1@gmail.com>
2023-01-09 07:19:29 -0300

    PDFmanual

--- assets/app.js
+++ assets/app.js
@@ -1,42 +1,46 @@
 
-
+// import html2canvas from '/html2canva';
 
 const boton= document.querySelector(".pdf");
+const ContenedorPDF= document.querySelector(".wrapper");
+
+
+boton.addEventListener("click",(e)=>{ 
+    //  alert("hola")
+    e.preventDefault()
+     Swal.fire({
+      position: 'top',
+      icon: 'success',
+      title: 'Puede contactarme cuando guste.gracias!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  
+    //  document.body
+        html2canvas(ContenedorPDF).then(function
+            (canvas) {
+
+                let constanteURL=canvas.toDataURL("image/png")
+   
 
 
 
-boton.addEventListener("click",(e)=>{  alert("hola")
+            let doc = new jsPDF('p','pt','a4');
+            
+                let width =doc.internal.pageSize.getWidth();
+                let height =canvas.height * width/ canvas.width;
+             
+                doc.addImage(constanteURL, 'PNG',-31,3, width+33, height+230)
+                doc.save("ouput.pdf")
+                
+                doc.addPage('p','pt','a4');
+                doc.addImage(constanteURL, 'PNG', -31, -height+443, width+33, height+238)
+               
+                doc.save("a4.pdf");
+                
 
-var doc = new jsPDF();
-doc.text("Olimpo geek", 10, 10);
-
-doc.line(10,10,180,10);
-doc.addPage("a4","paaa")
-doc.save("a4.pdf");
-
-
-
-})
-
-// console.log("Funciona");
-
-// function cal(){
-//     return Math.round( Math.random()*(10-0)+0);
-// }
-// var alumnos = ["Juan","pedro","matias"];
-
-
-
-
-
-
-
-// alumnos.forEach(alumno => {
-//     doc.addPage("a4","p");
-    
-//     var calificacion =cal();
-
-//     doc.text(alumno+" Calificación: "+calificacion.toString(),10,10)
-//     var imgData 
-// }
-
+            })
+          
+          
+       
+          })

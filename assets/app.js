bc2fe3507436d45e3f32c01c6e0f39c94b022ad3
wolfmarcos <estacread1@gmail.com>
2023-01-10 03:31:28 -0300

    pdf22

--- assets/app.js
+++ assets/app.js
@@ -6,48 +6,17 @@ const ContenedorPDF= document.querySelector(".wrapper");
 
 
 boton.addEventListener("click",(e)=>{ 
-    //  alert("hola")
-    e.preventDefault()
+
+
     Swal.fire({
-      html: 'https://wolfmarcos.github.io/CV-Marcos-Cicero/assets/images/CV_Marcos_Cicero.pdf',
-      imageHeight: 1500,
-      imageAlt: 'A tall image'
-    
-    
-    
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
     })
 
+    
 
-    //  Swal.fire({
-    //   position: 'left',
-    //   icon: 'success',
-    //   title: 'Puede contactarme cuando guste.gracias!',
-    //   showConfirmButton: false,
-    //   timer: 1500
-    // })
-  
-    //  document.body
-        // html2canvas(ContenedorPDF).then(function
-        //     (canvas) {
-
-        //         let constanteURL=canvas.toDataURL("image/png")
-   
-
-
-
-        //     let doc = new jsPDF('p','pt','legal');
-            
-        //         let width =doc.internal.pageSize.getWidth();
-        //         let height =canvas.height * width/ canvas.width;
-             
-        //         doc.addImage(constanteURL, 'PNG',18,-50, width-25, height-350)
-        //         doc.save("ouput.pdf")
-                
-            
-                
-
-        //     })
-          
-          
        
           })
